f9e9157fba215e75339187419b31b879f1baaf74
keval101 <kevalvadhiya101@gmail.com>
2021-07-09 12:17:18 +0530

    add dropdown ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.
 import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -22,11 +24,13 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
          RecipesDetailComponent,
          RecipesItemComponent,
          ShoppingListComponent,
-         ShoppingEditComponent
+         ShoppingEditComponent,
+         DropdownDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
